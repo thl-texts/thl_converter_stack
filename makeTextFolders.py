c5d7783dc0322f96b443fe969277407b5af8cf0d
ThanGrove <than@virginia.edu>
2023-08-04 12:23:03 -0400

    Adding script to create text folders for a collection with the format ####/coll-ed-####-text.xml

--- makeTextFolders.py
+++ makeTextFolders.py
@@ -1,25 +1,55 @@
 #!env/bin/python
 
 from os import listdir, makedirs
-from os.path import exists, join
+from os.path import exists, join, split
 from shutil import move
 import re
 
 DIRPATH = './workspace/out'
 PATT = r'^[^-]+-(\d{4})'
+DTD = '<?xml version="1.0" encoding="UTF-8"?>' \
+        '<!DOCTYPE TEI.2 SYSTEM "../../../../../xml/dtds/xtib3.dtd" [' \
+        '<!ENTITY % thlnotent SYSTEM "../../../catalog-refs.dtd" >' \
+        '%thlnotent;' \
+        ' <!ENTITY lccw-TNUM SYSTEM "../../0/lccw-TNUM-bib.xml">' \
+      ']><TEI.2'
+SRCPATT = '<sourceDesc (.*)</sourceDesc>'
+SRCNEW = ' <sourceDesc n="tibbibl">&lccw-TNUM;</sourceDesc>'
 
-files = [fnm for fnm in listdir(DIRPATH) if fnm.endswith('.xml')]
 
-for fnm in files:
-    # print(fnm)
-    mtch = re.search(PATT, fnm)
-    if mtch:
-        fldrnm = join(DIRPATH, mtch.group(1))
-        if not exists(fldrnm):
-            makedirs(fldrnm)
+def cleanup(src, dest, tnum):
+    with open(src, 'r', encoding='utf-8') as filein:
+        data = filein.read()
+        pts = data.split('<TEI.2')
+        newdata = DTD.replace('TNUM', tnum) + pts[1]
+
+        ndpts = newdata.split('<sourceDesc')
+        allnew = ndpts[0] + SRCNEW.replace('TNUM', tnum)
+        ndpts2 = ndpts[1].split('</sourceDesc>')
+        allnew = allnew + ndpts2[1]
+
+    [fpth, fnm] = split(dest)
+    if not exists(fpth):
+        makedirs(fpth)
+    with open(dest, 'w', encoding='utf-8') as fileout:
+        fileout.write(allnew)
+
+
+def main():
+    flsindir = listdir(DIRPATH)
+    files = [fnm for fnm in listdir(DIRPATH) if fnm.endswith('.xml')]
+    for fnm in files:
+        # print(fnm)
+        mtch = re.search(PATT, fnm)
+        if mtch:
+            tnum = mtch.group(1)
+            fldrnm = join(DIRPATH, tnum)
             src = join(DIRPATH, fnm)
             dest = join(fldrnm, fnm)
-            move(src, dest)
+            cleanup(src, dest, tnum)
+
+    print(f"Created text folders and moved texts into them in: {DIRPATH}")
 
 
-print(f"Created text folders and moved texts into them in: {DIRPATH}")
+if __name__ == '__main__':
+    main()
